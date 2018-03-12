e6204884189d405bb4fbe827caeaac5a41e4df75
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-03-12 20:35:12 +0100

    Fix Someday icon color + My projects

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -27,12 +27,14 @@ export class AppHome {
             <app-todo-list-item title="Today" icon="star" iconColor="tertiary" todos={[{title: "Todo 3", isOverdue: true}, {title: "Todo 4", isOverdue: false}, {title: "Todo 5", isOverdue: false}]}></app-todo-list-item>
             <app-todo-list-item title="Next" icon="albums" iconColor="medium"></app-todo-list-item>
             <app-todo-list-item title="Scheduled" icon="calendar" iconColor="danger"></app-todo-list-item>
-            <app-todo-list-item title="Someday" icon="ios-archive" iconColor="medium"></app-todo-list-item>
+            <ion-item tappable={true}>
+              <ion-icon slot="start" name="ios-archive" color="brown"></ion-icon>
+              <ion-label>Someday</ion-label>
+              <ion-note slot="end" class="todo-count">...maybe</ion-note>
+            </ion-item>
           </ion-list>
           <ion-list>
-            <app-todo-list-item title="My projects" icon="pricetags" iconColor="medium" todos={[{},{},{},{},{},{}]}></app-todo-list-item>
-            <app-todo-list-item title="My project" icon="pricetag" iconColor="success"></app-todo-list-item>
-            <app-todo-list-item title="My projects" icon="cube" iconColor="success"></app-todo-list-item>
+            <app-todo-list-item title="My projects" icon="cube" iconColor="medium" todos={[{},{},{},{},{}]}></app-todo-list-item>
           </ion-list>
           {/* <stencil-route-link url='/profile/stencil'>
             <ion-button>
